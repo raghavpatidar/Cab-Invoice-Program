90a89fb019381a632aaed478552d216a6cb825c9
raghavpatidar <raghavpatidar997764@gmail.com>
2023-12-29 19:48:15 +0530

    Calculate-Fare

--- src/main/java/com/example/CabInvoice.java
+++ src/main/java/com/example/CabInvoice.java
@@ -1,7 +1,12 @@
 package com.example;
 
 public class CabInvoice {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Cab invoice program");
+    private static double FARE_PER_KM = 10.0;
+    private static double FARE_PER_MINUTE = 1.0;
+    private static double MINIMUM_FARE = 5.0;
+
+    public double calculateFare(double distance, double time) {
+        return Math.max(MINIMUM_FARE, ((distance * FARE_PER_KM) + (time * FARE_PER_MINUTE)));
     }
+
 }
