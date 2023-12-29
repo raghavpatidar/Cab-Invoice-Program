87212fe064d6b66f42c304095efe1c88391df7e1
raghavpatidar <raghavpatidar997764@gmail.com>
2023-12-29 20:36:32 +0530

    DAY25 Done

--- src/main/java/com/example/CabInvoice.java
+++ src/main/java/com/example/CabInvoice.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.List;
+
 public class CabInvoice {
     private static double FARE_PER_KM = 10.0;
     private static double FARE_PER_MINUTE = 1.0;
@@ -9,4 +11,14 @@ public class CabInvoice {
         return Math.max(MINIMUM_FARE, ((distance * FARE_PER_KM) + (time * FARE_PER_MINUTE)));
     }
 
+    public Invoice generateInvoiceForUserRides(List<Ride> rides) {
+        double totalFare = 0;
+        for (Ride ride : rides) {
+            ride.setFare(calculateFare(ride.getDistance(), ride.getTime()));
+            totalFare += ride.getFare();
+            System.out.println(ride);
+        }
+
+        return new Invoice(rides.size(), totalFare, totalFare / rides.size());
+    }
 }
